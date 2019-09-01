60c223f33b00c5b1664093a19d03524c00bbdea2
genzouw <genzouw@gmail.com>
2019-09-01 10:38:44 +0900

    コードフォーマット適用.

--- Controller/PagesController.php
+++ Controller/PagesController.php
@@ -12,21 +12,20 @@
  * Redistributions of files must retain the above copyright notice.
  *
  * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
- * @link          http://cakephp.org CakePHP(tm) Project
- * @package       app.Controller
+ *
+ * @see          http://cakephp.org CakePHP(tm) Project
  * @since         CakePHP(tm) v 0.2.9
+ *
  * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
-
 App::uses('AppController', 'Controller');
 
 /**
- * Static content controller
+ * Static content controller.
  *
  * Override this controller by placing a copy in controllers directory of an application
  *
- * @package       app.Controller
- * @link http://book.cakephp.org/2.0/en/controllers/pages-controller.html
+ * @see http://book.cakephp.org/2.0/en/controllers/pages-controller.html
  */
 class PagesController extends AppController
 {
